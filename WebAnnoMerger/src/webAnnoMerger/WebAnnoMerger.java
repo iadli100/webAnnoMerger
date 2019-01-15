fa958ca278cf6fb6ea81db88e7adcc5b59936529
GermanConstructicon <GermanConstructicon@germ4-22.phil-fak.uni-duesseldorf.de>
2019-01-15 14:34:39 +0100

    first working version. :)

--- WebAnnoMerger/src/webAnnoMerger/WebAnnoMerger.java
+++ WebAnnoMerger/src/webAnnoMerger/WebAnnoMerger.java
@@ -9,157 +9,177 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WebAnnoMerger {
 
-	private static String LINE = "1-1\t...\t[111]\t[222][333]\n";
-
 	/**
 	 * patterns to parse the .tsv webanno exports.
 	 */
-		private static final Pattern PATTERN_SENTENCE_INDEX = Pattern.compile("(\\d+-\\d+)");
-		private static final Pattern PATTERN_SENTENCE_INDEX_DETAILED = Pattern.compile("(\\d+)(-\\d+)");
-		private static final Pattern PATTERN_LABELS = Pattern.compile("\\[(\\d+)\\]");
+	private static final Pattern PATTERN_SENTENCE_INDEX = Pattern.compile("(\\d+-\\d+)");
+	private static final Pattern PATTERN_SENTENCE_INDEX_DETAILED = Pattern.compile("(\\d+)(-\\d+)");
+	private static final Pattern PATTERN_LABELS = Pattern.compile("\\[(\\d+)\\]");
+
+	private static final Boolean DEBUG = false;
+
+	private static StringBuilder webAnnoMetaInformation = new StringBuilder();
 
 	public static void main(String[] args) throws Exception, FileNotFoundException {
-		System.out.printf("%s is set up.\n", "webAnnoMerger");
+		if (DEBUG)
+			System.out.printf("%s is set up.\n", "webAnnoMerger");
 
 		StringBuilder output = new StringBuilder();
 
-		// 1. read file
-		File f = new File("resources/dwds_export_Aussage_steht_1.tsv");
-		File f2 = new File("resources/dwds_export_Aussage_steht_2.tsv");
-
-		ArrayList<String> lines = readFile(f);
-		lines.addAll(readFile(f2));
-		
+		ArrayList<String> lines = mergeInputFiles(args);
 
 		// 2. parse lines
 		int globalEntryIndex = 0;
-		for(String line : lines) {
+		for (String line : lines) {
 			// skip newline and # except #Text=
 			if (line.isEmpty() || (line.startsWith("#") && !line.startsWith("#Text="))) {
-				// 
-				// TODO: add unprocessed line to global output file
-				//
-				output.append(line);
+				output.append(line + "\n");
 				continue;
-			} 
-			else if (line.startsWith("#Text=")) {
-				// 
-				// TODO: add unprocessed line to global output file
-				//
-				output.append(line);
+			} else if (line.startsWith("#Text=")) {
+				output.append(line + "\n");
 				globalEntryIndex++;
+				continue;
 			}
-			
+
 			// replace local index with global index
-			String result = line;
-			Matcher indexMatcher = PATTERN_SENTENCE_INDEX.matcher(line);
-			if (indexMatcher.find()) {
-				System.out.printf("\nglobal_index: %d local_index: %s", globalEntryIndex, indexMatcher.group(1)); // TODO: convert to DEBUG-log
-				
-				Matcher detailedIndexMatcher = PATTERN_SENTENCE_INDEX_DETAILED.matcher(indexMatcher.group(1));
-				if (detailedIndexMatcher.find()) {
-					// System.out.printf("%s %s\n", detailedIndexMatcher.group(1), detailedIndexMatcher.group(2));
-					
-					String.format("%d-%s\n", globalEntryIndex, detailedIndexMatcher.group(2));
-					
-					result = result.replaceFirst(indexMatcher.group(1), String.format("%d%s", globalEntryIndex, detailedIndexMatcher.group(2)));
-				}
-			}
-			
-			
+			String result = replaceLocalIndexWithGlobalIndex(line, globalEntryIndex);
+
 			// replace local unique label with global unique label
-			Matcher labelsMatcher = PATTERN_LABELS.matcher(line);
-			System.out.print(" local_labels: "); // TODO: convert to DEBUG
-			while (labelsMatcher.find()) {
-				System.out.print(labelsMatcher.group(1) + " "); // TODO: convert to DEBUG
+			result = replaceLocalUniqueLabelWithGlobalUniqueLabel(result, globalEntryIndex);
 
-				// result = result.replaceFirst(labelsMatcher.group(1), globalEntryIndex + "" + labelsMatcher.group(1));
-				// result = result.replaceAll(labelsMatcher.group(1), globalEntryIndex + "" + labelsMatcher.group(1)); // tweaking the right pattern
-
-				// result = result.replaceFirst("\\[" + labelsMatcher.group(1) + "\\]", "\\[" + globalEntryIndex + "" + labelsMatcher.group(1) + "\\]");
-				result = result.replaceFirst(String.format("\\[%s\\]", labelsMatcher.group(1)), String.format("\\[%d%s\\]", globalEntryIndex, labelsMatcher.group(1)));
-			}
-			System.out.println(); // TODO: convert to DEBUG
-			System.out.println(result); // TODO: add processed line to output
-			
-			output.append(result);
-			
-			/*
-			Pattern index = Pattern.compile("(\\d-\\d)");
-			Pattern labels = Pattern.compile("\\[(\\d+)\\]");
-
-			Matcher matcher = labels.matcher(LINE);
-
-			while (matcher.find()) {
-				System.out.println(matcher.group(1));
-				System.out.println("replacement: " + matcher.group(1).replaceAll(matcher.group(1), "i" + matcher.group(1)));
-				String result = LINE.replaceAll(matcher.group(1), "i" + matcher.group(1));
-				System.out.println(result);
-				System.out.print("Start index: " + matcher.start());
-				System.out.print(" End index: " + matcher.end() + " ");
-				System.out.println(matcher.group());
-			}
-			// */
-			
+			output.append(result + "\n");
 		}
-
 
 		// 3. generate output
 		writeOutput(output.toString());
-
-		// now create a new pattern and matcher to replace whitespace with tabs
-		/*
-		 * Pattern replace = Pattern.compile("\\s+"); Matcher matcher2 =
-		 * replace.matcher(LINE); System.out.println(matcher2.replaceAll("\t")); //
-		 */
 	}
 
-	private static ArrayList<String> readFile(File f) throws IOException {
+	/**
+	 * takes .jar args and treats them as file names. merges all files into array of
+	 * strings.
+	 * 
+	 * @param args .jar args treated as file names
+	 * @return all files merges into an array of strings
+	 * @throws IOException
+	 */
+	private static ArrayList<String> mergeInputFiles(String[] args) throws IOException {
 		ArrayList<String> lines = new ArrayList<String>();
-		
+
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+			File f = new File(args[i]);
+			lines.addAll(readFile(f));
+		}
+
+		return lines;
+	}
+
+	/**
+	 * take line from webAnno export and turns all local labels, e.g. [222] to
+	 * global unique labels like [x222].
+	 * 
+	 * @param line             of webAnno export with local unique labels, e.g.
+	 *                         [222]
+	 * @param globalEntryIndex global entry index for merged document.
+	 * @return
+	 */
+	private static String replaceLocalUniqueLabelWithGlobalUniqueLabel(String line, int globalEntryIndex) {
+		String result = line;
+
+		if (DEBUG)
+			System.out.print(" local_labels: ");
+
+		Matcher labelsMatcher = PATTERN_LABELS.matcher(line);
+		while (labelsMatcher.find()) {
+			if (DEBUG)
+				System.out.print(labelsMatcher.group(1) + " ");
+
+			/*
+			 * result = result.replaceFirst("\\[" + labelsMatcher.group(1) + "\\]", "\\[" +
+			 * globalEntryIndex + "" + labelsMatcher.group(1) + "\\]");
+			 */
+			result = result.replaceFirst(String.format("\\[%s\\]", labelsMatcher.group(1)),
+					String.format("\\[%d%s\\]", globalEntryIndex, labelsMatcher.group(1)));
+		}
+		if (DEBUG)
+			System.out.println();
+		return result;
+	}
+
+	/**
+	 * takes line from webAnno export and changes local entry index to global entry
+	 * index.
+	 * 
+	 * @param line             from webAnno export.
+	 * @param globalEntryIndex global entry index for merged document.
+	 * @return
+	 */
+	private static String replaceLocalIndexWithGlobalIndex(String line, int globalEntryIndex) {
+		String result = line;
+
+		Matcher indexMatcher = PATTERN_SENTENCE_INDEX.matcher(line);
+		if (indexMatcher.find()) {
+			if (DEBUG)
+				System.out.printf("\nglobal_index: %d local_index: %s", globalEntryIndex, indexMatcher.group(1));
+
+			Matcher detailedIndexMatcher = PATTERN_SENTENCE_INDEX_DETAILED.matcher(indexMatcher.group(1));
+			if (detailedIndexMatcher.find()) {
+				/* 
+				result = result.replaceFirst(indexMatcher.group(1),
+						String.format("%d%s", globalEntryIndex, detailedIndexMatcher.group(2)));
+						// */
+				result = result.replaceAll(indexMatcher.group(1),
+						String.format("%d%s", globalEntryIndex, detailedIndexMatcher.group(2)));
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * reads webAnno export and drops first 13 lines being web anno meta info. TODO:
+	 * please consider the magic number 13. it may vary for future versions of
+	 * webanno.
+	 * 
+	 * @param f file
+	 * @return
+	 * @throws IOException
+	 */
+	private static ArrayList<String> readFile(File f) throws IOException {
+		webAnnoMetaInformation = new StringBuilder(); // redirect meta information into separate variable
+
+		ArrayList<String> lines = new ArrayList<String>();
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f), "utf-8"));
 		String line;
 
 		int lineIdx = 0;
-		int corpusIDCounter = 0;
 		while ((line = br.readLine()) != null) {
 			++lineIdx;
 
-			System.out.printf("%d: %s\n", lineIdx, line);
-			lines.add(line);
-
-			line = line.trim();
-			if (!line.isEmpty() && !line.startsWith("#")) {
-				++corpusIDCounter;
-
-				// String[] entries = PATTERN_SEARCH_TAB.split(line);
-
-				// if (entries.length != 6) {
-
-				// String timestamp = entries[1].trim();
-
-				// if (timeStampMatcher.find()) {
-
-				// timestamp = timeStampMatcher.group(3) + "." + timeStampMatcher.group(2) + "."
-				// + timeStampMatcher.group(1);
-
-				// if (id.isEmpty() || id.equals("_"))
-
-				// id = String.valueOf(corpusIDCounter);
+			if (line.startsWith("#T_SP=") || line.startsWith("#FORMAT=")) {
+				webAnnoMetaInformation.append(line + "\n");
+				continue;
 			}
 
+			if (DEBUG)
+				System.out.printf("%d: %s\n", lineIdx, line);
+			lines.add(line);
+
+			/*
+			 * if (id.isEmpty() || id.equals("_")) id = String.valueOf(corpusIDCounter);
+			 */
 		}
+
+		br.close();
 		return lines;
 	}
 
@@ -170,10 +190,14 @@ public class WebAnnoMerger {
 	 * @throws IOException
 	 */
 	private static void writeOutput(String output) throws IOException {
+		// get time stamp for file name
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream("resources/" + timeStamp + "_webanno.tsv"), StandardCharsets.UTF_8));
-		bw.write(output.toString());
+				new FileOutputStream(timeStamp + "_webanno.tsv"), StandardCharsets.UTF_8));
+
+		bw.write(webAnnoMetaInformation.toString());
+		bw.write(output);
 		bw.flush();
 		bw.close();
 	}
